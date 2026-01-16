66f380ecd94a134207f2edece2acea85413b8968
emirfikri <emir_fikrir@yahoo.com>
2026-01-17 01:44:13 +0800

    update corsheader

--- src/app/api/helper.ts
+++ src/app/api/helper.ts
@@ -1,11 +1,6 @@
-const allowedOrigins = [
-    "https://ai.emirfikri.com",
-    "http://localhost:3000",
-];
-
 export function corsHeaders() {
     return {
-        'Access-Control-Allow-Origin': allowedOrigins,
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type',
     };
